462adc7321ce65e3bf032743746e7e5bcd7edf39
jburgapem <jennifer.burga@gmail.com>
2025-09-17 15:20:15 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,16 +3,6 @@
 # Compatible con OIDC (GitHub Actions) #
 ########################################
 
-variable "primary_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "secondary_region" {
-  type    = string
-  default = "us-west-2"
-}
-
 # Provider AWS primario
 provider "aws" {
   alias  = "primary"
