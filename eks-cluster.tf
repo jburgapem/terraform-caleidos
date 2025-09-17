06a6316486832e9c69cdd59652c333216784f2b6
jburgapem <jennifer.burga@gmail.com>
2025-09-17 13:02:58 -0500

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,15 +1,13 @@
 module "eks_primary" {
-  source    = "terraform-aws-modules/eks/aws"
-  providers = { aws = aws.primary }
-  version   = "20.13.0"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.13.0"
+  providers       = { aws = aws.primary }
 
   cluster_name    = "${var.cluster_name}-primary"
   cluster_version = "1.29"
-  vpc_id          = aws_vpc.primary.id
-  subnet_ids      = [
-    aws_subnet.primary_private_a.id,
-    aws_subnet.primary_private_b.id,
-  ]
+
+  vpc_id     = var.primary_vpc_id
+  subnet_ids = var.primary_private_subnets
 
   eks_managed_node_groups = {
     default = {
@@ -19,20 +17,24 @@ module "eks_primary" {
       instance_types = ["t3.medium"]
     }
   }
+
+  # Evitar crear KMS y CloudWatch logs
+  create_kms_key              = false
+  cluster_log_group_enabled   = false
+  manage_aws_auth             = true
+  enable_irsa                 = true
 }
 
 module "eks_secondary" {
-  source    = "terraform-aws-modules/eks/aws"
-  providers = { aws = aws.secondary }
-  version   = "20.13.0"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.13.0"
+  providers       = { aws = aws.secondary }
 
   cluster_name    = "${var.cluster_name}-secondary"
   cluster_version = "1.29"
-  vpc_id          = aws_vpc.secondary.id
-  subnet_ids      = [
-    aws_subnet.secondary_private_a.id,
-    aws_subnet.secondary_private_b.id,
-  ]
+
+  vpc_id     = var.secondary_vpc_id
+  subnet_ids = var.secondary_private_subnets
 
   eks_managed_node_groups = {
     default = {
@@ -42,4 +44,9 @@ module "eks_secondary" {
       instance_types = ["t3.medium"]
     }
   }
+
+  create_kms_key              = false
+  cluster_log_group_enabled   = false
+  manage_aws_auth             = true
+  enable_irsa                 = true
 }
