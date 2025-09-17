9716b526ad1f6a1532deb6bbd3624c96f0cfab45
jburgapem <jennifer.burga@gmail.com>
2025-09-17 15:29:30 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,6 @@
-variable "cluster_name" {
-  type    = string
-  default = "acme-eks"
-}
-
 variable "primary_vpc_id" {
-  type = string
+  default = "vpc-0abc123456789def0"
 }
-
 variable "primary_private_subnets" {
-  type = list(string)
-}
-
-variable "secondary_vpc_id" {
-  type = string
-}
-
-variable "secondary_private_subnets" {
-  type = list(string)
+  default = ["subnet-0123456789abcdef0", "subnet-0fedcba9876543210"]
 }
