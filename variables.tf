4a466b0da1b8653d1a5c1efeeb3d6dd99f37e5a4
jburgapem <jennifer.burga@gmail.com>
2025-09-17 15:40:40 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,3 +23,14 @@ variable "cluster_name" {
   type        = string
   default     = "acme-eks"
 }
+variable "primary_region" {
+  description = "AWS region for the primary cluster"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "secondary_region" {
+  description = "AWS region for the secondary cluster"
+  type        = string
+  default     = "us-west-2"
+}
