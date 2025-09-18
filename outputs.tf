4e4693153d9a18f2090244cd6481916e9aa34452
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-17 22:28:29 -0500

    Creando Cluster

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "eks_primary_cluster_id" {
-  value = module.eks_primary.cluster_id
-}
-
-# output "eks_secondary_cluster_id" {
-#   value = module.eks_secondary.cluster_id
+# output "eks_primary_cluster_id" {
+#   value = module.eks_primary.cluster_id
 # }
+
+output "eks_secondary_cluster_id" {
+  value = module.eks_secondary.cluster_id
+}
