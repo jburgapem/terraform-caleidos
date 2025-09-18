6765f98dd1b3ecda221845d791d00898b8ee9eae
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-17 22:19:39 -0500

    Creando Cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,7 @@
 module "eks_primary" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.13.0"
-  providers       = { aws = aws.primary }
+  # providers       = { aws = aws.primary }
 
   cluster_name    = "${var.cluster_name}-primary"
   cluster_version = "1.29"
@@ -22,26 +22,26 @@ module "eks_primary" {
   enable_irsa    = true
 }
 
-module "eks_secondary" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.13.0"
-  providers       = { aws = aws.secondary }
+# module "eks_secondary" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   version         = "20.13.0"
+#   providers       = { aws = aws.secondary }
 
-  cluster_name    = "${var.cluster_name}-secondary"
-  cluster_version = "1.29"
+#   cluster_name    = "${var.cluster_name}-secondary"
+#   cluster_version = "1.29"
 
-  vpc_id     = aws_vpc.secondary.id
-  subnet_ids = [aws_subnet.secondary_private_a.id, aws_subnet.secondary_private_b.id]
+#   vpc_id     = aws_vpc.secondary.id
+#   subnet_ids = [aws_subnet.secondary_private_a.id, aws_subnet.secondary_private_b.id]
 
-  eks_managed_node_groups = {
-    default = {
-      desired_size   = 2
-      max_size       = 4
-      min_size       = 1
-      instance_types = ["t3.medium"]
-    }
-  }
+#   eks_managed_node_groups = {
+#     default = {
+#       desired_size   = 2
+#       max_size       = 4
+#       min_size       = 1
+#       instance_types = ["t3.medium"]
+#     }
+#   }
 
-  create_kms_key = true
-  enable_irsa    = true
-}
+#   create_kms_key = true
+#   enable_irsa    = true
+# }
