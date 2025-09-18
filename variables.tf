9cfc725ed899ac6126123b31ae1d65eac97050bc
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 11:39:25 -0500

    Agregando Nodos

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "cluster_name" {
   type        = string
   default     = "acme-eks" # cámbialo al nombre base que quieras
 }
+
 variable "secondary_vpc_id" {
   description = "VPC ID for the secondary cluster"
   type        = string
@@ -11,4 +12,16 @@ variable "secondary_vpc_id" {
 variable "secondary_private_subnets" {
   description = "Private subnets for the secondary cluster"
   type        = list(string)
+}
+
+variable "primary_region" {
+  description = "AWS region for the primary cluster"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "secondary_region" {
+  description = "AWS region for the secondary cluster"
+  type        = string
+  default     = "us-west-2"
 }
