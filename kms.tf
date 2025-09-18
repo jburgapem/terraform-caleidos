9af403f326c9f8b87a1b504b787006aca49aa8f4
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 13:27:11 -0500

    Creando Cluster EKS

--- kms.tf
+++ kms.tf
@@ -1,21 +1,20 @@
-resource "aws_kms_key" "primary" {
-  description             = "KMS key for EKS primary cluster"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
+#####################################
+# KMS KEYS
+#####################################
+module "kms_primary" {
+  source  = "terraform-aws-modules/kms/aws"
+  version = "1.1.0"
+
+  description        = "KMS key for primary EKS cluster"
+  aliases            = ["eks/primary"]
+  enable_key_rotation = true
 }
 
-resource "aws_kms_alias" "primary" {
-  name          = "alias/${var.cluster_name}-primary-key"
-  target_key_id = aws_kms_key.primary.key_id
-}
+module "kms_secondary" {
+  source  = "terraform-aws-modules/kms/aws"
+  version = "1.1.0"
 
-resource "aws_kms_key" "secondary" {
-  description             = "KMS key for EKS secondary cluster"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
-}
-
-resource "aws_kms_alias" "secondary" {
-  name          = "alias/${var.cluster_name}-secondary-key"
-  target_key_id = aws_kms_key.secondary.key_id
+  description        = "KMS key for secondary EKS cluster"
+  aliases            = ["eks/secondary"]
+  enable_key_rotation = true
 }
