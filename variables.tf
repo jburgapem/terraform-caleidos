4610aaf58bb374a91da3893d48764b684ea696ee
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 11:36:05 -0500

    Agregando Nodos

--- variables.tf
+++ variables.tf
@@ -1,27 +1,9 @@
-variable "primary_region" {
-  description = "Región del cluster primario"
-  type        = string
-  default     = "us-east-2"
-}
-
-variable "secondary_region" {
-  description = "Región del cluster secundario"
-  type        = string
-  default     = "us-west-2"
-}
-
-variable "cluster_name" {
-  description = "Nombre base de los clusters EKS"
-  type        = string
-  default     = "acme-eks"
-}
-
-variable "primary_vpc_id" {
-  description = "VPC ID for the primary EKS cluster"
+variable "secondary_vpc_id" {
+  description = "VPC ID for the secondary cluster"
   type        = string
 }
 
-variable "primary_private_subnets" {
-  description = "Private subnet IDs for the primary EKS cluster"
+variable "secondary_private_subnets" {
+  description = "Private subnets for the secondary cluster"
   type        = list(string)
 }
