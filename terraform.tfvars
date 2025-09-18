734d4f88e40b6299959312e9dbb578ffadaf7702
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 00:12:28 -0500

    Creando Cluster

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,14 @@
-cluster_name              = "acme"
-#primary_region            = "us-east-1"
-secondary_region          = "us-west-2"
-#primary_vpc_id            = "vpc-123abc"
-#primary_private_subnets   = ["subnet-1a", "subnet-1b"]
-secondary_vpc_id          = "vpc-456def"
-secondary_private_subnets = ["subnet-2a", "subnet-2b"]
+# Regiones
+primary_region   = "us-east-1"
+secondary_region = "us-west-2"
+
+# Nombre base de los clusters
+cluster_name = "acme-eks"
+
+# VPC primaria y subnets privadas
+primary_vpc_id           = "vpc-0abc123456789def0"
+primary_private_subnets  = ["subnet-0aaa1111", "subnet-0bbb2222"]
+
+# VPC secundaria y subnets privadas
+secondary_vpc_id         = "vpc-0fedcba9876543210"
+secondary_private_subnets = ["subnet-0ccc3333", "subnet-0ddd4444"]
--- variables.tf
+++ variables.tf
@@ -1,19 +1,47 @@
+#############################
 # Región y nombres de clusters
-
+#############################
 variable "primary_region" {
-  type = string
+  description = "Región de AWS para el cluster primario"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "secondary_region" {
-  type = string
+  description = "Región de AWS para el cluster secundario"
+  type        = string
+  default     = "us-west-2"
 }
 
-# VPC y subnets primario
+variable "cluster_name" {
+  description = "Nombre base para los clusters EKS"
+  type        = string
+  default     = "acme-eks"
+}
 
+#############################
+# VPC y subnets primario
+#############################
 variable "primary_vpc_id" {
-  type = string
+  description = "ID de la VPC primaria"
+  type        = string
 }
 
 variable "primary_private_subnets" {
-  type = list(string)
+  description = "Subnets privadas de la VPC primaria"
+  type        = list(string)
 }
+
+#############################
+# VPC y subnets secundario
+#############################
+variable "secondary_vpc_id" {
+  description = "ID de la VPC secundaria"
+  type        = string
+}
+
+variable "secondary_private_subnets" {
+  description = "Subnets privadas de la VPC secundaria"
+  type        = list(string)
+}
+
