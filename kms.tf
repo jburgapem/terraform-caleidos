077d22b0c71c5aa6f2f421fdee08e62894c7218d
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 09:56:27 -0500

    creando Cluster EKS

--- kms.tf
+++ kms.tf
@@ -19,10 +19,3 @@ resource "aws_kms_alias" "secondary" {
   name          = "alias/${var.cluster_name}-secondary-key"
   target_key_id = aws_kms_key.secondary.key_id
 }
-
-module "kms_secondary" {
-  source     = "terraform-aws-modules/kms/aws"
-  version    = "3.1.0"
-  create_key = true
-  alias_name = "${var.cluster_name}-secondary-key"
-}
