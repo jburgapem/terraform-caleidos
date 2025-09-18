3de562152203ef89a989f3125ddc44a585394b1b
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-17 22:27:09 -0500

    Creando Cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,37 +1,13 @@
-module "eks_primary" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.13.0"
-  # providers       = { aws = aws.primary }
-
-  cluster_name    = "${var.cluster_name}-primary"
-  cluster_version = "1.29"
-
-  vpc_id     = aws_vpc.primary.id
-  subnet_ids = [aws_subnet.primary_private_a.id, aws_subnet.primary_private_b.id]
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size   = 2
-      max_size       = 4
-      min_size       = 1
-      instance_types = ["t3.medium"]
-    }
-  }
-
-  create_kms_key = true
-  enable_irsa    = true
-}
-
-# module "eks_secondary" {
+# module "eks_primary" {
 #   source          = "terraform-aws-modules/eks/aws"
 #   version         = "20.13.0"
-#   providers       = { aws = aws.secondary }
+#   # providers       = { aws = aws.primary }
 
-#   cluster_name    = "${var.cluster_name}-secondary"
+#   cluster_name    = "${var.cluster_name}-primary"
 #   cluster_version = "1.29"
 
-#   vpc_id     = aws_vpc.secondary.id
-#   subnet_ids = [aws_subnet.secondary_private_a.id, aws_subnet.secondary_private_b.id]
+#   vpc_id     = aws_vpc.primary.id
+#   subnet_ids = [aws_subnet.primary_private_a.id, aws_subnet.primary_private_b.id]
 
 #   eks_managed_node_groups = {
 #     default = {
@@ -45,3 +21,27 @@ module "eks_primary" {
 #   create_kms_key = true
 #   enable_irsa    = true
 # }
+
+module "eks_secondary" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.13.0"
+  providers       = { aws = aws.secondary }
+
+  cluster_name    = "${var.cluster_name}-secondary"
+  cluster_version = "1.29"
+
+  vpc_id     = aws_vpc.secondary.id
+  subnet_ids = [aws_subnet.secondary_private_a.id, aws_subnet.secondary_private_b.id]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      max_size       = 4
+      min_size       = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  create_kms_key = true
+  enable_irsa    = true
+}
