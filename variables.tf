54619ef444747277e94d3604938f535282b2e9de
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 11:28:39 -0500

    Agregando Nodos

--- variables.tf
+++ variables.tf
@@ -25,13 +25,3 @@ variable "primary_private_subnets" {
   description = "Private subnet IDs for the primary EKS cluster"
   type        = list(string)
 }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
