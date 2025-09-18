79ee42aeed75013518f6333f66d21842f24933f9
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 09:58:28 -0500

    creando Cluster EKS

---
+++ iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "fargate_primary" {
+  name = "${var.cluster_name}-primary-fargate-pod-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.fargate_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_primary" {
+  role       = aws_iam_role.fargate_primary.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "fargate_secondary" {
+  name = "${var.cluster_name}-secondary-fargate-pod-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.fargate_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_secondary" {
+  role       = aws_iam_role.fargate_secondary.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "fargate_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks-fargate-pods.amazonaws.com"]
+    }
+  }
+}
