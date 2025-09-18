fdbac458978c9cf1eba5da6018e1692e42a64a26
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-18 13:24:52 -0500

    Creando Cluster EKS

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,82 +1,85 @@
 #####################################
-# Cluster EKS Primario
+# EKS PRIMARY CLUSTER
 #####################################
 module "eks_primary" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.13.0"
-
   providers = {
     aws = aws.primary
   }
 
-  cluster = {
-    name    = "${var.cluster_name}-primary"
-    version = var.eks_version
+  cluster_name    = "${var.cluster_name}-primary"
+  cluster_version = "1.29"
+
+  vpc_id     = aws_vpc.primary.id
+  subnet_ids = [
+    aws_subnet.primary_private_a.id,
+    aws_subnet.primary_private_b.id
+  ]
+
+  # Seguridad
+  cluster_security_group_id             = aws_security_group.eks_primary_cluster.id
+  cluster_additional_security_group_ids = [aws_security_group.eks_primary_nodes.id]
+
+  # Encriptación con KMS
+  cluster_encryption_config = {
+    resources        = ["secrets"]
+    provider_key_arn = module.kms_primary.key_arn
   }
 
-  vpc_id     = var.primary_vpc_id
-  subnet_ids = var.primary_private_subnets
-
-  cluster_security_group_id = aws_security_group.eks_primary_cluster.id
-  node_security_group_ids   = [aws_security_group.eks_primary_nodes.id]
-
+  # Node groups
   eks_managed_node_groups = {
     default = {
-      desired_size   = 2
-      max_size       = 4
       min_size       = 1
+      max_size       = 4
+      desired_size   = 2
       instance_types = ["t3.medium"]
     }
   }
 
-  encryption_config = {
-    resources = ["secrets"]
-    provider_key_arn = module.kms_primary.key_arn
-  }
-
   enable_irsa = true
-
-  tags = var.tags
 }
 
 #####################################
-# Cluster EKS Secundario
+# EKS SECONDARY CLUSTER
 #####################################
 module "eks_secondary" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.13.0"
-
   providers = {
     aws = aws.secondary
   }
 
-  cluster = {
-    name    = "${var.cluster_name}-secondary"
-    version = var.eks_version
+  cluster_name    = "${var.cluster_name}-secondary"
+  cluster_version = "1.29"
+
+  vpc_id     = aws_vpc.secondary.id
+  subnet_ids = [
+    aws_subnet.secondary_private_a.id,
+    aws_subnet.secondary_private_b.id
+  ]
+
+  # Seguridad
+  cluster_security_group_id             = aws_security_group.eks_secondary_cluster.id
+  cluster_additional_security_group_ids = [aws_security_group.eks_secondary_nodes.id]
+
+  # Encriptación con KMS
+  cluster_encryption_config = {
+    resources        = ["secrets"]
+    provider_key_arn = module.kms_secondary.key_arn
   }
 
-  vpc_id     = var.secondary_vpc_id
-  subnet_ids = var.secondary_private_subnets
-
-  cluster_security_group_id = aws_security_group.eks_secondary_cluster.id
-  node_security_group_ids   = [aws_security_group.eks_secondary_nodes.id]
-
+  # Node groups
   eks_managed_node_groups = {
     default = {
-      desired_size   = 2
-      max_size       = 4
       min_size       = 1
+      max_size       = 4
+      desired_size   = 2
       instance_types = ["t3.medium"]
     }
   }
 
-  encryption_config = {
-    resources = ["secrets"]
-    provider_key_arn = module.kms_secondary.key_arn
-  }
-
   enable_irsa = true
-
-  tags = var.tags
 }
+
 
