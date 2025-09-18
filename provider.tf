47e3fa4376edc438bcdb66dfd921438c612d8144
Jennifer Burga <jennifer.burga@gmail.com>
2025-09-17 21:54:14 -0500

    Agragando Backend

--- provider.tf
+++ provider.tf
@@ -18,3 +18,9 @@ provider "aws" {
   region = var.secondary_region
   profile = "jen"
 }
+
+backend "s3" {
+    bucket = "backend-state-jn-dev"
+    key    = "dev/eks.tfstate"
+    region = "us-east-2"
+  }
